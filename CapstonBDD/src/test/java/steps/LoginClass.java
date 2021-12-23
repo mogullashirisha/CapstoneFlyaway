0de84b6255587154215f418ef671be019805b0df
mogullashirisha <mogullashirisha57@gmail.com>
2021-12-23 16:20:36 +0530

    first commit

---
+++ CapstonBDD/src/test/java/steps/LoginClass.java
@@ -0,0 +1,22 @@
+package steps;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginClass {
+	WebDriver driver;
+
+	public LoginClass(WebDriver driver) {
+		// TODO Auto-generated constructor stub
+		this.driver=driver;
+	}
+	public void Login() {
+		driver.findElement(By.name("email_id")).sendKeys("shireesha@domain.com");
+		driver.findElement(By.name("pwd")).sendKeys("shireesha");
+		System.out.println("user opened login page and enter details");
+		driver.findElement(By.xpath("//button[text()='Login']")).click();
+		System.out.println("user clicks on login button");
+		System.out.println("user is on login page");
+	}
+}
+
+
---
+++ CapstonBDD/src/test/java/steps/SignupClass.java
@@ -0,0 +1,36 @@
+package steps;
+
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class SignupClass {
+	WebDriver driver;
+
+	public SignupClass(WebDriver driver) {
+		// TODO Auto-generated constructor stub
+		this.driver=driver;
+	}
+
+	public void SignUp() {
+
+		driver.findElement(By.xpath("//a[text()='Login/Signup']")).click();
+		System.out.println("user has opened login page");
+		driver.findElement(By.xpath("//a[text()='Not a member? Signup']")).click();
+		System.out.println("user opts to register");
+		driver.findElement(By.name("email_id")).sendKeys("shireesha@domain.com");
+		driver.findElement(By.name("pwd")).sendKeys("shireesha");
+		driver.findElement(By.name("pwd2")).sendKeys("shireesha");
+		driver.findElement(By.name("name")).sendKeys("shireesha");
+		System.out.println("user enters details required");
+		driver.findElement(By.xpath("//button[text()='Signup']")).click();
+		driver.findElement(By.xpath("//a[text()='Already a member? Login']")).click();
+		System.out.println("user registered successfully");
+
+
+	}
+}
+
+
+
